3d4d56daa278292bc2c9e9797e6776e44323c7a2
esiaero <anthias@seas.upenn.edu>
2022-04-21 17:39:15 -0400

    comment cleanup + typo fix

--- hlsystem/FLUIDPlugin.C
+++ hlsystem/FLUIDPlugin.C
@@ -1,9 +1,5 @@
-
-
-
 #include <UT/UT_DSOVersion.h>
 //#include <RE/RE_EGLServer.h>
-
 
 #include <UT/UT_Math.h>
 #include <UT/UT_Interrupt.h>
@@ -24,23 +20,10 @@ using namespace HDK_Sample;
 
 #define FRAME_RANGE 100
 
-//
-// Help is stored in a "wiki" style text file. 
-//
-// See the sample_install.sh file for an example.
-//
-// NOTE : Follow this tutorial if you have any problems setting up your visual studio 2008 for Houdini 
-//  http://www.apileofgrains.nl/setting-up-the-hdk-for-houdini-12-with-visual-studio-2008/
-
-
-///
-/// newSopOperator is the hook that Houdini grabs from this dll
-/// and invokes to register the SOP.  In this case we add ourselves
-/// to the specified operator table.
-///
-void
-newSopOperator(OP_OperatorTable *table)
-{
+// newSopOperator is the hook that Houdini grabs from this dll
+// and invokes to register the SOP.  In this case we add ourselves
+// to the specified operator table.
+void newSopOperator(OP_OperatorTable *table) {
     table->addOperator(
 	    new OP_Operator("CusFluid",			// Internal name
 			    "Hydro-Gen2O",			// UI name
@@ -52,38 +35,22 @@ newSopOperator(OP_OperatorTable *table)
 			     OP_FLAG_GENERATOR)		// Flag it as generator
 	    );
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//PUT YOUR CODE HERE
-//You need to declare your parameters here
-//Example to declare a variable for angle you can do like this :
+//declare your parameters here
 static PRM_Name		constraintIteration("constraintIteration", "Constraint Iteration");
 static PRM_Name		artificialPressure("artificialPressure", "Artificial Pressure");
 static PRM_Name		viscosity("viscosity", "Viscosity");
 static PRM_Name		vorticityConfinement("vorticityConfinement", "Vorticity Confinement");
 static PRM_Name		timeFrame("timeFrame", "Time Frame");
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //				     ^^^^^^^^    ^^^^^^^^^^^^^^^
 //				     internal    descriptive version
 
-
-// PUT YOUR CODE HERE
-// You need to setup the initial/default values for your parameters here
-// For example : If you are declaring the inital value for the angle parameter
+// initial/default values for your parameters here
 static PRM_Default constraintIterationDefault(2);
 static PRM_Default artificialPressureDefault(0.0001);
 static PRM_Default viscosityDefault(0.01);
 static PRM_Default vorticityConfinementDefault(0.0003);
 static PRM_Default timeFrameDefault(0);
-
 
 static PRM_Range iterationRange(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_RESTRICTED, 30);
 static PRM_Range tensileRange(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_RESTRICTED, 0.01);
@@ -91,33 +58,21 @@ static PRM_Range viscosityRange(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_RESTRICTED, 0
 static PRM_Range vorticityRange(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_RESTRICTED, 0.001);
 static PRM_Range timeFrameRange(PRM_RANGE_RESTRICTED, 0, PRM_RANGE_RESTRICTED, FRAME_RANGE);
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
 PRM_Template
 SOP_Fluid::myTemplateList[] = {
-// PUT YOUR CODE HERE
-// You now need to fill this template with your parameter name and their default value
-// EXAMPLE : For the angle parameter this is how you should add into the template
+// default vals
 PRM_Template(PRM_INT,	PRM_Template::PRM_EXPORT_MIN, 1, &constraintIteration, &constraintIterationDefault, 0, &iterationRange),
 PRM_Template(PRM_FLT,	PRM_Template::PRM_EXPORT_MIN, 1, &artificialPressure, &artificialPressureDefault, 0, &tensileRange),
 PRM_Template(PRM_FLT,	PRM_Template::PRM_EXPORT_MIN, 1, &viscosity, &viscosityDefault, 0, &viscosityRange),
 PRM_Template(PRM_FLT,	PRM_Template::PRM_EXPORT_MIN, 1, &vorticityConfinement, &vorticityConfinementDefault, 0, &vorticityRange),
 PRM_Template(PRM_INT,	PRM_Template::PRM_EXPORT_MIN, 1, &timeFrame, &timeFrameDefault, 0, &timeFrameRange),
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
     PRM_Template()
 };
 
-
 // Here's how we define local variables for the SOP.
 enum {
-	VAR_PT,		// Point number of the star
-	VAR_NPT,	// Number of points in the star
+	VAR_PT,		// Point number
+	VAR_NPT,	// Number of points
 	VAR_FS,  // my pointer to my fluid system
 	VAR_OI, //iteration
 	VAR_OK, //artificial pressure kCorr
@@ -137,17 +92,13 @@ SOP_Fluid::myVariables[] = {
     { 0, 0, 0 },
 };
 
-bool
-SOP_Fluid::evalVariableValue(fpreal &val, int index, int thread)
-{
+bool SOP_Fluid::evalVariableValue(fpreal &val, int index, int thread) {
     // myCurrPoint will be negative when we're not cooking so only try to
     // handle the local variables when we have a valid myCurrPoint index.
-    if (myCurrPoint >= 0)
-    {
+    if (myCurrPoint >= 0) {
 	// Note that "gdp" may be null here, so we do the safe thing
 	// and cache values we are interested in.
-	switch (index)
-	{
+	switch (index) {
 	    case VAR_PT:
 		val = (fpreal) myCurrPoint;
 		return true;
@@ -174,15 +125,12 @@ SOP_Fluid::evalVariableValue(fpreal &val, int index, int thread)
     return SOP_Node::evalVariableValue(val, index, thread);
 }
 
-OP_Node *
-SOP_Fluid::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
-{
+OP_Node* SOP_Fluid::myConstructor(OP_Network *net, const char *name, OP_Operator *op) {
     return new SOP_Fluid(net, name, op);
 }
 
 SOP_Fluid::SOP_Fluid(OP_Network *net, const char *name, OP_Operator *op)
-	: SOP_Node(net, name, op)
-{
+	: SOP_Node(net, name, op) {
     myCurrPoint = -1;	// To prevent garbage values from being returned
 	myFS = new FluidSystem();
 	myFS->SPH_CreateExample(0, 0);
@@ -195,10 +143,8 @@ SOP_Fluid::SOP_Fluid(OP_Network *net, const char *name, OP_Operator *op)
 	oldVorticity = 0.0003;
 }
 
-void SOP_Fluid::runSimulation(int frameNumber)
-{
+void SOP_Fluid::runSimulation(int frameNumber) {
 	totalPos.clear();
-
 	for (int i = 0; i <= frameNumber; ++i)
 	{
 		myFS->Run();
@@ -215,27 +161,18 @@ void SOP_Fluid::runSimulation(int frameNumber)
 
 SOP_Fluid::~SOP_Fluid() {}
 
-unsigned
-SOP_Fluid::disableParms()
-{
+unsigned SOP_Fluid::disableParms() {
     return 0;
 }
 
 OP_ERROR
 SOP_Fluid::cookMySop(OP_Context &context)
 {
-	fpreal		 now = context.getTime();
-
-	// PUT YOUR CODE HERE
-	// Decare the necessary variables and get always keep getting the current value in the node
-	// For example to always get the current angle thats set in the node ,you need to :
-	//    float angle;
-	//    angle = ANGLE(now)       
-    //    NOTE : ANGLE is a function that you need to use and it is declared in the header file to update your values instantly while cooking 
+	fpreal now = context.getTime();
 	//FluidSystem myplant;
 
 	int ite;
-	ite = CONSTARIANT_ITERATION(now);
+	ite = CONSTRAINT_ITERATION(now);
 
 	float kCorr;
 	kCorr = ARTIFICIAL_PRESSURE(now);
@@ -261,18 +198,10 @@ SOP_Fluid::cookMySop(OP_Context &context)
 		runSimulation(FRAME_RANGE);
 	}
 
-	/*UT_String grammarFileUT;
-	evalString(grammarFileUT, "grammarFile", 0, now);
-	std::string grammarFile;
-	grammarFile = grammarFileUT.toStdString();*/
-
 	/*std::cout << "iteration " << ite << endl;
 	cout << "stiffnes " << stif << endl;
 	cout << "viscosity " << visc << endl;
 	cout << "vorticity " << vorticity << "\n" << endl;*/
-
-
-	///////////////////////////////////////////////////////////////////////////
 
 	//PUT YOUR CODE HERE
 	// Next you need to call your Lystem cpp functions 
@@ -281,47 +210,26 @@ SOP_Fluid::cookMySop(OP_Context &context)
 	//myplant.setDefaultAngle(angle);
 	//myplant.setDefaultStep(stepSize);
 
-
-
-	///////////////////////////////////////////////////////////////////////////////
-
-	// PUT YOUR CODE HERE
 	// You the need call the below function for all the genrations ,so that the end points points will be
 	// stored in the branches vector , you need to declare them first
-
 	
 	/*std::vector<LSystem::Branch> branches = std::vector<LSystem::Branch>();
 	myplant.process(iterations, branches);*/
 
 	//myFS->Run();
-
-
-	///////////////////////////////////////////////////////////////////////////////////
-
-
-	// Now that you have all the branches ,which is the start and end point of each point ,its time to render 
-	// these branches into Houdini 
-    
-
-	// PUT YOUR CODE HERE
-	// Declare all the necessary variables for drawing cylinders for each branch 
     float		 rad, tx, ty, tz;
     int			 divisions, plane;
     int			 xcoord =0, ycoord = 1, zcoord =2;
     float		 tmp;
-    UT_Vector4		 pos;
+    UT_Vector4	 pos;
     GU_PrimPoly		*poly;
     int			 i;
     UT_Interrupt	*boss;
 
-
     // Since we don't have inputs, we don't need to lock them.
-
-    divisions  = 5;	// We need twice our divisions of points
+    divisions = 5;	// We need twice our divisions of points
     myTotalPoints = divisions;		// Set the NPT local variable value
-    myCurrPoint   = 0;			// Initialize the PT local variable
-
-
+    myCurrPoint = 0;			// Initialize the PT local variable
 
     // Check to see that there hasn't been a critical error in cooking the SOP.
     if (error() < UT_ERROR_ABORT)
@@ -377,8 +285,6 @@ SOP_Fluid::cookMySop(OP_Context &context)
 				GA_Offset ptoffend = poly->getPointOffset(1);
 				gdp->setPos3(ptoffend, posUp);
 			}
-			////////////////////////////////////////////////////////////////////////////////////////////
-
 			// Highlight the star which we have just generated.  This routine
 			// call clears any currently highlighted geometry, and then it
 			// highlights every primitive for this SOP. 
@@ -389,7 +295,6 @@ SOP_Fluid::cookMySop(OP_Context &context)
 		// regardless of what opStart() returns.
 		boss->opEnd();
     }
-
     myCurrPoint = -1;
     return error();
 }
--- hlsystem/FLUIDPlugin.h
+++ hlsystem/FLUIDPlugin.h
@@ -1,38 +1,28 @@
-
-
 #ifndef __FLUID_PLUGIN_h__
 #define __FLUID_PLUGIN_h__
 
 //#include <GEO/GEO_Point.h>
-//
 #include <SOP/SOP_Node.h>
 #include "fluid_system.h"
 
 namespace HDK_Sample {
-class SOP_Fluid : public SOP_Node
-{
+class SOP_Fluid : public SOP_Node {
 public:
-    static OP_Node		*myConstructor(OP_Network*, const char *,
-							    OP_Operator *);
-
+    static OP_Node *myConstructor(OP_Network*, const char *name, OP_Operator *op);
     /// Stores the description of the interface of the SOP in Houdini.
     /// Each parm template refers to a parameter.
     static PRM_Template		 myTemplateList[];
 
     /// This optional data stores the list of local variables.
     static CH_LocalVariable	 myVariables[];
-
 protected:
-
     SOP_Fluid(OP_Network *net, const char *name, OP_Operator *op);
     virtual ~SOP_Fluid();
 
     /// Disable parameters according to other parameters.
     virtual unsigned		 disableParms();
 
-
-    /// cookMySop does the actual work of the SOP computing, in this
-    /// case, a LSYSTEM
+    /// cookMySop does the actual work of the SOP computing
     virtual OP_ERROR		 cookMySop(OP_Context &context);
 
     /// This function is used to lookup local variables that you have
@@ -41,9 +31,10 @@ protected:
 				    fpreal &val,
 				    int index,
 				    int thread);
+
     // Add virtual overload that delegates to the super class to avoid
     // shadow warnings.
-    virtual bool		 evalVariableValue(
+    virtual bool evalVariableValue(
 				    UT_String &v,
 				    int i,
 				    int thread)
@@ -54,21 +45,15 @@ protected:
     void runSimulation(int frameNumber);
 
 private:
-    /// The following list of accessors simplify evaluating the parameters
-    /// of the SOP.
+    /// The following list of accessors simplify evaluating the parameters of the SOP.
 
-    // PUT YOUR CODE HERE
-	// Here you need to declare functions which need to be called from the .C file to 
-	// constantly update the cook function, these functions help you get the current value that the node has
-	// Example : To declare a function to fetch angle you need to do it this way 
-    exint   CONSTARIANT_ITERATION(exint t) { return evalInt("constraintIteration", 0, t); }
-    fpreal  ARTIFICIAL_PRESSURE(fpreal t) { return evalFloat("artificialPressure", 0, t); }
+	// functions to constantly update the cook function, get the current value that the node has
+    exint CONSTRAINT_ITERATION(exint t) { return evalInt("constraintIteration", 0, t); }
+    fpreal ARTIFICIAL_PRESSURE(fpreal t) { return evalFloat("artificialPressure", 0, t); }
 
-    fpreal  VISCOSITY(fpreal t) { return evalFloat("viscosity", 0, t); }
-    fpreal  VORTICITY_CONFINEMENT(fpreal t) { return evalFloat("vorticityConfinement", 0, t); }
+    fpreal VISCOSITY(fpreal t) { return evalFloat("viscosity", 0, t); }
+    fpreal VORTICITY_CONFINEMENT(fpreal t) { return evalFloat("vorticityConfinement", 0, t); }
     exint TIME_FRAME(exint t) { return evalInt("timeFrame", 0, t); }
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /// Member variables are stored in the actual SOP, not with the geometry
     /// In this case these are just used to transfer data to the local 
@@ -86,5 +71,4 @@ private:
     std::vector<std::vector<glm::dvec3>> totalPos; // all postions at all frame (0-60)
 };
 } // End HDK_Sample namespace
-
 #endif
